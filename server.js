f110e190cbb54c981a192b71e14eda2ce9b81c85
anthonykam <ant@gmail.com>
2024-10-26 18:12:37 +0800

    complete http request get

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const port=2000;
+const connt=express();
+const conntPath=path.dirname(fileURLToPath(import.meta.url));
+
+connt.use("/static",express.static(path.join(conntPath,"static")));
+
+connt.listen(port,()=>{
+    console.log(`port ${port} connected...`);
+})
+
+connt.get("/",(req,res)=>{
+    res.sendFile(path.join(conntPath,"login.html"));
+})
